5816c458ae5438e6b9969be5c88b971dd8606fa2
Tarun1026 <tarun08531234@gmail.com>
2024-11-08 01:08:16 +0530

    credentials

--- client/src/hooks/GetUserDetails.jsx
+++ client/src/hooks/GetUserDetails.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
-
+const apiUrl = import.meta.env.VITE_API_URL;
 const getUserDetail = async () => {
-  const apiUrl = import.meta.env.VITE_API_URL;
+  
   try {
     const response = await axios.get(`${apiUrl}/api/users/current-user-details`, {
-      withCredentials: true // Make sure cookies are sent with the request
+      // withCredentials: true 
     });
 
     if (response.status === 200) {
