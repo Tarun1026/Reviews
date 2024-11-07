e2ebb5cc8e4e239680216ba98b22d844aefb91e1
Tarun1026 <tarun08531234@gmail.com>
2024-11-07 22:38:01 +0530

    Vercel link

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app=express()
 
 app.use(cors({
-    origin: 'https://reviews-c2hf-client.vercel.app/', // Replace with your actual frontend URL
+    origin: 'https://reviews-c2hf-client.vercel.app', 
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
