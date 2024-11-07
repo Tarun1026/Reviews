97527cf1209493b665d77aa7d3a96f28d3305706
Tarun1026 <tarun08531234@gmail.com>
2024-11-08 00:13:42 +0530

    Methods rmv

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,7 @@ import cookieParser from "cookie-parser"
 const app=express()
 
 app.use(cors({
-    origin: 'https://reviews-coral.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    origin: 'https://reviews-coral.vercel.app', 
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
