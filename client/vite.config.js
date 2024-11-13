22c476cbe5674d55419f6d965de0d5f046a41c3b
Tarun1026 <tarun08531234@gmail.com>
2024-11-13 17:59:22 +0530

    getUser

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api/users':"https://reviews-xlzv.onrender.com"
+      '/api/users':"http://localhost:4000"
     }
    },
   plugins: [react()],
