301b72558a35592ca0b90051c4c67430d4a1ea1a
Tarun1026 <tarun08531234@gmail.com>
2025-01-13 10:51:40 +0530

    pass

--- server/src/middleware/Email.confiq.js
+++ server/src/middleware/Email.confiq.js
@@ -1,12 +1,15 @@
 import nodemailer from "nodemailer"
-
+import dotenv from "dotenv"
+dotenv.config({
+    path:"./.env"
+})
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false, // true for port 465, false for other ports
     auth: {
       user: "tarun08531234@gmail.com",
-      pass: "mkpx loul vjrd cogg",
+      pass: `${process.env.EMAIL_PASS}`,
     },
   });
 
